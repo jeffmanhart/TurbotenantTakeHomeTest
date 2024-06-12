f190153ad452261cad115bcaf35b1c3fd75a3066
Jeffrey Manhart <jeffreymanhart@gmail.com>
2024-06-12 15:03:11 -0600

    functioning test

--- test/pageobjects/property.page.js
+++ test/pageobjects/property.page.js
@@ -9,6 +9,9 @@ class PropertyPage extends Page {
     get addPropertyButton () {
         return $('div.header > button:nth-child(1)');
     }
+    async getProperty(title) {
+        return $(`h3=${title}`)
+    }
 
     get titleInput () {
         return $('div.addForm > input[type=text]:nth-child(1)');
@@ -17,18 +20,24 @@ class PropertyPage extends Page {
         return $('div.addForm > input[type=text]:nth-child(2)');
     }
     get rentInput () {
-        return $('div.addForm > input[type=text]:nth-child(3)');
+        return $('#root > div > div > div.addForm > input[type="number"]:nth-child(3)');
     }
 
     get btnSubmit () {
             return $('button=Submit');
         }
 
+    get successMessage (){
+        return $('.message-success')
+    }
+
     async addProperty(title, description, rent){
+        await this.addPropertyButton.click();
         await this.titleInput.setValue(title);
         await this.descriptionInput.setValue(description);
         await this.rentInput.setValue(rent);
         await this.btnSubmit.click();
+        await this.successMessage.waitForExist()
     }
 }
 
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,13 +1,19 @@
 import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import PropertyPage from '../pageobjects/property.page.js'
+import propertyPage from '../pageobjects/property.page.js'
+
+let uniqueString = Date.now();
 
 describe('Add a new property', () => {
     it('should save and display the new property', async () => {
         await LoginPage.open()
-        await LoginPage.login('jmtest', '12345!')
-        await PropertyPage.addProperty('autoTitle', 'new desc', 25)
-        debug();
+        await LoginPage.login('', '')
+        await propertyPage.addPropertyButton.waitForExist()
+        await PropertyPage.addProperty(`autoTitle ${uniqueString}`, `new desc ${uniqueString}`, 25)
+        await PropertyPage.btnSubmit.waitForExist({ reverse: true })
+        await browser.pause(500)
+        await expect(propertyPage.getProperty(`autoTitle ${uniqueString}`)).toExist()
     })
 })
 
